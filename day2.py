6e7e0e1f2384dbca8f3bd59f99e123c6cdec295c
Shivani Pal <shivanipal770@gmail.com>
2026-06-18 20:59:46 +0530

    day2

---
+++ day2.py
@@ -0,0 +1,17 @@
+#Print all the even number between 1 and 100
+# num =2
+# while num <=100:
+#     print(num)
+#     num =num +2
+
+# number=1
+# while number<=100:
+#     if number % 2 ==0:
+#         print(number)
+#     number +=1
+
+#Print all the odd numbers between 1 and 100
+numbers =1
+while numbers<=100:
+    print(numbers)
+    numbers +=2
