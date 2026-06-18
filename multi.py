801f280bcf4b754eae48676cd7c299a410b11d09
Shivani Pal <shivanipal770@gmail.com>
2026-06-18 21:33:31 +0530

    natural

---
+++ multi.py
@@ -0,0 +1,16 @@
+ # multiplication table from \(1\) to \(10\) using a while loop
+# n = int(input("Enter the number table you want : "))
+# i=1
+# while i<=10:
+#     print(n, "x", i, "=", n * i)
+#     i+=1
+
+
+#print the sum of first n natural numbers
+n = int(input("Enter the numbers of your choice "))
+sum=0
+i=1
+while i<n :
+    sum +=i
+    i+=1
+    print(f"The sum of the first {n} natural numbers is: {sum}")
